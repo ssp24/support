402abcd50bee43e56bd285aea8b526487d4c41b5
ssp24 <62755563+ssp24@users.noreply.github.com>
2022-07-11 17:36:34 +0200

    Update oai-setnames.py

--- oai-setnames.py
+++ oai-setnames.py
@@ -12,6 +12,31 @@ import unicodedata
 data = pd.read_csv("oai_setnames.csv", encoding = "utf-8")
 data = data.drop(["Unnamed: 0.1", "Unnamed: 0"], axis=1)
 
+
+choice = st.selectbox(
+            "Folgende übergeordnete OAI-Sets stehen zur Verfügung:", 
+            ('Deutsche Nationalbibliografie (DNB)', 
+             'Deutsche Nationalbibliografie (DNB) - APPR',
+             'Deutsche Nationalbibliografie: Monografien und Periodika des Verlagsbuchhandels',
+             'Deutsche Nationalbibliografie: Monografien und Periodika außerhalb des Verlagsbuchhandels',
+             'Deutsche Nationalbibliografie: Karten', 
+             'Deutsche Nationalbibliografie: Hochschulschriften',
+             'Deutsche Nationalbibliografie: Musikalien',
+             'Deutsche Nationalbibliografie: Online-Publikationen',
+             'Deutsche Nationalbibliografie: Musiktonträger',
+             'Deutsche Nationalbibliografie: Kataloganreicherung (TOC-Service)',
+             'Deutsche Nationalbibliografie (nur Datensaetze nach abgeschlossener Bearbeitung)',
+             'Deutsche Nationalbibliografie (nur Datensaetze nach abgeschlossener Bearbeitung): Publikationen des Verlagsbuchhandels',
+             'Deutsche Nationalbibliografie (nur Datensaetze nach abgeschlossener Bearbeitung): Publikationen ausserhalb des Verlagsbuchhandels',
+             'Deutsche Nationalbibliografie (nur Datensaetze nach abgeschlossener Bearbeitung): Karten',
+             'Deutsche Nationalbibliografie (nur Datensaetze nach abgeschlossener Bearbeitung): Hochschulschriften',
+             'Deutsche Nationalbibliografie (nur Datensaetze nach abgeschlossener Bearbeitung): Musikalien',
+             'Deutsche Nationalbibliografie (nur Datensaetze nach abgeschlossener Bearbeitung): Online-Publikationen',
+             'Deutsche Nationalbibliografie: Monografien und Periodika des Verlagsbuchhandels - APPR',          
+            )
+            
+
+
 dnbcolor = ['#FEFEFE', '#2499ff', '#f33930', '#b6c73f', '#ffd44d',
             '#3cb8f6', '#f9852e', '#e3d98f', '#000000', '#01be00']
 testcolor = ['#ff6900', '#fcb900', '#7bdcb5', '#00d084', '#8ed1fc',
