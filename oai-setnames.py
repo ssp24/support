acab26f6f3dbc954c634abe84b611f343510405f
ssp24 <62755563+ssp24@users.noreply.github.com>
2022-07-15 14:00:32 +0200

    Update oai-setnames.py

--- oai-setnames.py
+++ oai-setnames.py
@@ -14,13 +14,6 @@ data = data.drop(["Unnamed: 0.1", "Unnamed: 0"], axis=1)
 main = data["Main"].drop_duplicates()
 
           
-
-dnbcolor = ['#FEFEFE', '#2499ff', '#f33930', '#b6c73f', '#ffd44d',
-            '#3cb8f6', '#f9852e', '#e3d98f', '#000000', '#01be00']
-testcolor = ['#ff6900', '#fcb900', '#7bdcb5', '#00d084', '#8ed1fc',
-            '#0693e3', '#abb8c3', '#eb144c', '#f78da7', '#9900ef']
-
-
 st.header('Übersicht OAI-Sets der DNB')
 
 st.write("Hier finden Sie eine Übersicht der über die OAI-Schnittstelle zur Verfügung stehenden Sets: ") 
@@ -30,9 +23,6 @@ test = st.selectbox(
           "Folgende übergeordnete OAI-Sets stehen zur Verfügung:", 
           (main))
 
-#st.write('You selected:', test)
-
-#st.dataframe(data)
 
 if test: 
           selection = data[["Second", "Set"]].loc[data["Main"] == test]
@@ -43,15 +33,3 @@ else:
 
 st.write("Stand der Daten: 11.07.2022")
 
-
-
-#Jahre: 
-#data = data[data['Year'].notna()]
-#data = data.astype({'Year':'int'})
-#data = data[(data['Year'] >= 1900) & (data['Year'] <= 2100)]
-
-#s = data['Year'].value_counts()[:33].sort_index()
-#fig = px.bar(s, labels={'index':'Jahr', 'value':'Anzahl'}, color='value', height=500)
-#st.plotly_chart(fig, use_container_width=True)
-
-#st.info("INFO: Es werden die Daten für die Jahre 1990 bis 2022 (laufend) dargestellt. " ) 
